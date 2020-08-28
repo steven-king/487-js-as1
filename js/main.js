f3a3c6efb1569333ab6fecb1f15c97d1ed2e125c
steven-king <steven.king@unc.edu>
2020-08-28 14:07:01 -0400

    adding files

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//In the space below each problem, write the code to answer. This page connects to index.html
+
+/* 1.--------
+ Write the JavaScript to display the current day and time in the following format and output it to the html page to the span with an id of "question-1"
+*/
+
+
+
+/* 2.--------
+
+*/
+
+
+
+
+/* 3.--------
+
+*/
+
+
+
+
+
+/* 4.--------
+
+*/
