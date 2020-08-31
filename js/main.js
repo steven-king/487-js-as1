ca2cc0fc7ab51033db351306e04f0290f6e2e311
annamac7 <annamac7@live.unc.edu>
2020-08-31 13:24:49 -0400

    my change

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //In the space below each problem, write the code to answer. This page connects to index.html
 // Dont forget to do the PR after you are done.
-/* 1.--------
+/* 1. Making a change for example
  Write the JavaScript to display the current day and time in the following format and output it to the html page to the span with an id of "question-1"
 */
 
