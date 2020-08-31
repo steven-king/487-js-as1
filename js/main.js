afbd442e785d824165491781fd95c2c8153205ec
maureenosei <mosei@live.unc.edu>
2020-08-31 13:07:11 -0400

    minor changes to js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@
 /* 1.--------
  Write the JavaScript to display the current day and time in the following format and output it to the html page to the span with an id of "question-1"
 */
-
+var myDateVar = new Date();
+console.log(myDateVar);
+document.getElementById("answer1").innerHTML = myDateVar;
 
 
 /* 2.--------
