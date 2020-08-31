73a0dda6a1dab17eed1345d4223219e6af3cb354
atdundas <atdsch150@gmail.com>
2020-08-31 11:38:45 -0400

    08.31.2020 11:38am

--- js/main.js
+++ js/main.js
@@ -3,23 +3,50 @@
 /* 1.--------
  Write the JavaScript to display the current day and time in the following format and output it to the html page to the span with an id of "question-1"
 */
+var fullDate = new Date();
+var listDay = [
+	'Sunday',
+	'Monday',
+	'Tuesday',
+	'Wednesday',
+	'Thursday',
+	'Friday',
+	'Saturday',
+];
+var currentDay = listDay[fullDate.getDay()];
+//code acquired from
+//https://stackoverflow.com/questions/8888491/how-do-you-display-javascript-datetime-in-12-hour-am-pm-format
+var currentTime = fullDate.toLocaleString('en-US', {
+	hour: 'numeric',
+	minute: 'numeric',
+	second: 'numeric',
+	hour12: true,
+});
 
+console.log(currentDay, currentTime);
 
+document.getElementById('answer1').innerHTML =
+	'Today is ' + currentDay + '</br>' + 'Current Time: ' + currentTime;
 
 /* 2.--------
 
 */
+var currentYear = fullDate.getFullYear();
+var currentMonth = fullDate.getMonth();
+var currentDay = fullDate.getDay();
 
+var format1 = currentMonth + '-' + currentDay + '-' + currentYear;
+var format2 = currentMonth + '/' + currentDay + '/' + currentYear;
+var format3 = currentDay + '/' + currentMonth + '/' + currentYear;
 
+console.log(format1, format2, format3);
 
+document.getElementById('answer2').innerHTML =
+	format1 + '</br>' + format2 + '</br>' + format3;
 
 /* 3.--------
 
 */
-
-
-
-
 
 /* 4.--------
 
