30aa6b5e627a3d0c9461917bfe95e6715e9fa758
addy98 <addisonwest98@gmail.com>
2020-09-01 10:55:46 -0400

    question 4 done

--- js/main.js
+++ js/main.js
@@ -30,3 +30,18 @@ function myFunction() {
 /* 4.--------
 
 */
+
+paper.install(window);
+paper.setup(document.getElementById('main-canvas'));
+
+function helloWorld() {
+  var c = Shape.Circle(200, 200, 80);
+  c.fillColor = 'black';
+  var text = new PointText(200, 200);
+  text.justification = 'center';
+  text.fillColor = 'white';
+  text.fontSize = 20;
+  text.content = 'hello world';
+}
+
+paper.view.draw();
