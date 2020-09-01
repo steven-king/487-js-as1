1d6b8daa9011fe29a48937e45f90995e2f17af00
jojoyang0515 <jojoyang0515@gmail.com>
2020-09-01 19:54:31 -0400

    Update Questions

--- js/main.js
+++ js/main.js
@@ -3,50 +3,45 @@
 
 /* 1.--------Write the JavaScript to display the current day and time in the following format and output it to the html page to the span with an id of "answer1"*/
 //adapted from https://stackoverflow.com/questions/18536726/javascript-to-display-the-current-date-and-time
-//today's date
-var today = new Date(); 
+var today = new Date();
 
 //display the current weekday
-var array = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
-var weekday = array[today.getDate()+1];
+var array = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var weekday = array[today.getDate() + 1];
 var hour = today.getHours();
 var minute = today.getMinutes();
 var second = today.getSeconds();
 
 //if minute hand is less than 10 min, it adds a zero in the from (e.g. 3 minutes is :03, not :3)
-if (today.getMinutes() < 10){
+if (today.getMinutes() < 10) {
     minute = "0" + minute;
 }
 
 //if second hand is less than 10 sec, it adds a zero in the from (e.g. 3 seconds is :03, not :3)
-if (today.getSeconds() < 10){
-   second = "0" + second;
+if (today.getSeconds() < 10) {
+    second = "0" + second;
 }
 
-//generates the current time in the correct format
-var currentTime = hour % 12+ ":" + minute + ":" + second; 
+//Show the time in AM/PM format. Adapted from https://www.geeksforgeeks.org/how-do-you-display-javascript-datetime-in-12-hour-am-pm-format/
+var TwelveHourFormat = hour >= 12 ? " PM" : " AM";
 
-//tells the system when to display AM and PM
-if (hour < 12) {
-    currentTime += " AM";
-}
-if (hour > 12) { 
-    currentTime += " PM";
-}
+
+//generates the current time in the correct order/format
+var currentTime = hour % 12 + ":" + minute + ":" + second + TwelveHourFormat;
 
 //Answer
-document.getElementById('answer1').innerHTML = "Today is " + weekday + ". <br> Current Time : " + currentTime; 
+document.getElementById('answer1').innerHTML = "Today is " + weekday + ". <br> Current Time : " + currentTime;
 
 console.log(weekday);
 console.log(currentTime);
 
 /* 2.--------Write the JavaScript to display the current day and time in the following format and output it to the html page to the span with an id of "answer2"*/
 var yyyy = today.getFullYear();
-var mm = today.getMonth()+1;
+var mm = today.getMonth() + 1;
 var dd = today.getDate();
 
 //puts a zero before the mm digit if the month is Jan-Sep
-if (mm < 10){
+if (mm < 10) {
     mm = "0" + mm;
 }
 
@@ -56,11 +51,11 @@ if (dd < 10) {
 }
 
 //Answer
-document.getElementById('answer2').innerHTML = 
-    mm + "-" + dd + "-" + yyyy + 
-    "<br>" + 
-    mm + "/" + dd + "/" + yyyy + 
-    "<br>" + 
+document.getElementById('answer2').innerHTML =
+    mm + "-" + dd + "-" + yyyy +
+    "<br>" +
+    mm + "/" + dd + "/" + yyyy +
+    "<br>" +
     dd + "/" + mm + "/" + yyyy;
 
 console.log(mm);
@@ -70,22 +65,25 @@ console.log(yyyy);
 /* 3.--------Write an HTML button below and when it clicks, it prints/displays the content of the current html page (current window).*/
 //adapted from https://stackoverflow.com/questions/17636528/how-do-i-load-an-html-page-in-a-div-using-javascript
 function showHTML() {
+    
     //Answer
-    document.getElementById("html").innerHTML = 
+    document.getElementById("html").innerHTML =
         '<object type="text/html" data="index.html" ></object>';
 }
 
 console.log("Question 3: Display content in index.html");
 
 /* 4.--------Follow the instructions on Chapter 1 part Hello World and and Draw the graphics below in the id called main-canvas.*/
-paper.install(window); paper.setup(document.getElementById('main-canvas'));
+paper.install(window);
+paper.setup(document.getElementById('main-canvas'));
 //var c = Shape.Circle(100, 80, 70); 
 //c.fillColor = 'green';
-var c = Shape.Circle(200, 100, 80); 
+var c = Shape.Circle(200, 100, 80);
 c.fillColor = 'black';
-var text = new PointText(200, 100); 
-text.justification = 'center'; 
-text.fillColor = 'white'; text.fontSize = 20;
+var text = new PointText(200, 100);
+text.justification = 'center';
+text.fillColor = 'white';
+text.fontSize = 20;
 text.content = 'hello world';
 
 paper.view.draw();
