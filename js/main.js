ec39dd318c1be599e932d5e62e8d8f6b6e55f970
atamvakis <tamvakis@live.unc.edu>
2020-09-01 23:00:30 -0400

    Saving JS A1

--- js/main.js
+++ js/main.js
@@ -3,24 +3,60 @@
 /* 1.--------
  Write the JavaScript to display the current day and time in the following format and output it to the html page to the span with an id of "question-1"
 */
+var date = new Date();
+var week = new Array(7);
+week[0] = "Sunday";
+week[1] = "Monday";
+week[2] = "Tuesday";
+week[3] = "Wednesday";
+week[4] = "Thursday";
+week[5] = "Friday";
+week[6] = "Saturday";
+var weekDay = week[date.getDay()];
+
+var time = date.toLocaleTimeString();
+
+document.getElementById('answer1').innerHTML="Today is " + weekDay + "." + "<br />" + "Current Time : " + time;
+
+/* 2.--------*/
+var month = date.getMonth() + 1;
+var dateNum = date.getDate();
+var year = date.getFullYear();
+
+var dateOne = month + "-" + dateNum + "-" + year;
+var dateTwo = date.toLocaleDateString();
+var dateThree = dateNum + "/" + month + "/" + year;
+
+document.getElementById('answer2').innerHTML=dateOne + "<br />" + dateTwo + "<br />"+ dateThree;
+
+/* 3.--------*/
+function showCode(){
+  var code = document.documentElement.innerHTML;
+  console.log(code.toString());
+  var codeDisplay = code.toString();
+  document.getElementById('answer3').innerText=codeDisplay;
+}
 
 
 
-/* 2.--------
 
-*/
+/* 4.--------*/
+paper.install(window);
+paper.setup(document.getElementById('main-canvas'));
 
+var c = Shape.Circle(200, 200, 80);
+c.fillColor = 'black';
+var text = new PointText(200, 200);
+text.justification = 'center';
+text.fillColor = 'white';
+text.fontSize = '20';
+text.content = 'hello world';
 
+var tool1 = new Tool();
 
+tool1.onMouseDown = function(event) {
+  var c = Shape.Circle(event.point.x, event.point.y, 20);
+  c.fillColor = 'green';
+};
 
-/* 3.--------
-
-*/
-
-
-
-
-
-/* 4.--------
-
-*/
+paper.view.draw();
