a66f165c13fb95175bb0ee201c07b6fe07eafd77
yingxin <yingxin@live.unc.edu>
2020-09-01 22:37:29 -0400

    Yingxin's commit

--- js/main.js
+++ js/main.js
@@ -1,27 +1,103 @@
 //In the space below each problem, write the code to answer. This page connects to index.html
 // Dont forget to do the PR after you are done.
+
+
 /* 1.--------
  Write the JavaScript to display the current day and time in the following format and output it to the html page to the span with an id of "question-1"
 */
-// first commit//
-//branch commit//
+var today = new Date();
+var hours = today.getHours();
+
+/*if (hours == 24）{
+  hours = (hours - 24) + "AM";
+}*/
+if (hours == 0 ) {
+  hours = (hours - 12);
+  var time = "Current Time : "+ hours + ":" + today.getMinutes() + ":" + today.getSeconds() + " AM";
+}
+
+if (hours < 12 ) {
+  hours = hours;
+  var time = "Current Time : "+ hours + ":" + today.getMinutes() + ":" + today.getSeconds() + " AM";
+}
+if (hours >= 12 ) {
+  hours = (hours - 12);
+  var time = "Current Time : "+ hours + ":" + today.getMinutes() + ":" + today.getSeconds() + " PM";
+}
+if (hours == 12 ) {
+  hours = hours;
+  var time = "Current Time : "+ hours + ":" + today.getMinutes() + ":" + today.getSeconds() + " PM";
+}
+var weekday = "Today is " + getWeekDay(today) + ".";
+var daytime = weekday + "<br>" + time;
 
 
-/* 2.--------
+//Codes credits to https://thisinterestsme.com/javascript-get-day-of-week/
+function getWeekDay(today){
+    //Create an array containing each day, starting with Sunday.
+    var weekdays = new Array(
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    );
+    //Use the getDay() method to get the day.
+    var day = today.getDay();
+    //Return the element that corresponds to that index.
+    return weekdays[day];
+}
 
-*/
+document.getElementById('answer1').innerHTML = daytime;
+
+/* var c = Shape.Circle(200, 200, 50);
+c.fillColor = 'green';*/
 
 
+/* 2.--------*/
+
+var date = today.getDate();
+var month = today.getMonth() + 1;
+
+if (today.getMonth() < 9) {
+  month = "0"+ month;
+}
+
+if (today.getDate() < 10) {
+  date = "0" + date;
+}
+
+var date1 = (month) + '-' + date + '-' + today.getFullYear();
+var date2 = (month) + '/' + date + '/' + today.getFullYear();
+var date3 = date + '/' + (month) + '/' + today.getFullYear();
+var dateall = date1 + "<br>" + date2 + "<br>" + date3;
+document.getElementById('answer2').innerHTML = dateall;
 
 
-/* 3.--------
+/* 3.--------*/
 
-*/
+function load_home() {
+//Code credits to https://stackoverflow.com/questions/817218/how-to-get-the-entire-document-html-as-a-string#answer-35917295
+var markup = document.documentElement.innerHTML;
+document.getElementById('answer3').innerText = markup;
+}
 
+/* 4.--------*/
 
+$(document).ready(function () {
+ 'use strict';
+ paper.install(window);
+ paper.setup(document.getElementById("main-canvas"));
 
+ var tool = new Tool();
 
+ var c = Shape.Circle(100, 100, 80);
+ c.fillColor = 'black';
+ var text = new PointText(100, 100);
+ text.justification = 'center';
+ text.fillColor = 'white';
+ text.fontSize = 20;
+ text.content = 'hello world';
 
-/* 4.--------
-
-*/
+ tool.onMouseDown = function(event) {
+        var c = Shape.Circle(event.point.x, event.point.y, 20);
+        c.fillColor = 'green';
+    };
+ paper.view.draw();
+});
