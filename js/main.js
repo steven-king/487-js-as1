81ff2452023dbc4c31353394dbfeb5f0a55a42cd
will-melfi <willmelfi@icloud.com>
2020-09-01 23:42:59 -0400

    wow it worked

--- js/main.js
+++ js/main.js
@@ -48,8 +48,18 @@ function printpage() {
   paper.install(window);
   paper.setup(document.getElementById('mainCanvas'));
 
-  var c = Shape.Circle(200, 200, 50);
-  c.fillColor = 'green';
+  var tool = new Tool();
+  var c = Shape.Circle(200, 200, 80);
+  c.fillColor = 'black';
+  var text = new PointText(200, 200);
+  text.justification = 'center';
+  text.fillColor = 'white';
+  text.contSize = 20;
+  text.content = 'hello world';
+  tool.onMouseDown = function(event) {
+    var c = Shape.Circle(event.point.x, event.point.y, 20);
+    c.fillColor = 'green';
+  }
 
   paper.view.draw();
 }())
