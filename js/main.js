e6de09294c08cc7b131bf45e2892cf73b467a6a5
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-09-19 19:13:06 -0400

    task 4 completed

--- js/main.js
+++ js/main.js
@@ -76,8 +76,29 @@ function myFunction() {
 */
 //
 
+$(document).ready(function() {
+    'use strict';
+    paper.install(window);
+    paper.setup(document.getElementById('main-canvas'));
 
-
+    var c = Shape.Circle(200, 200, 80);
+    c.fillColor = 'black';
+    var text = new PointText(200, 200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 20;
+    text.content = 'hello world';
+    
+    var tool = new Tool();
+    tool.onMouseDown = function(event) {
+        var c = Shape.Circle(event.point, 20);
+        c.fillColor = 'green';
+    };
+       
+    
+    paper.view.draw();
+});
+   
 
 
 // adapt from Learning Javascript Ethan Brown Chapter 1
