d030eb505379fb6ba09bcabb59027132d4c5d3b2
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-09-19 18:17:59 -0400

    task 3 completed

--- js/main.js
+++ js/main.js
@@ -1,50 +1,12 @@
 var date = new Date();
-const WEEKDAY = getWeekdayAsString(date.getDay());
-const TIME = getAmPmString(date.getHours());
 
 /* 1.--------
  Write the JavaScript to display the current day and time in the following format and output it to the html page to the span with an id of "question-1"
 */
 
+const WEEKDAY = getWeekdayAsString(date.getDay());
+const TIME = getAmPmString(date.getHours());
 let date0 = `Today is ${WEEKDAY}.<br>Current Time: ${TIME}`;
-document.getElementById("answer1").innerHTML = date0;
-
-//adapt from https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date; https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
-
-
-
-/* 2.--------
-*/
-
-let date1 = `${date.getMonth() > 9 ? date.getMonth() : "0" + date.getMonth()}-${date.getDate()}-${date.getFullYear()}
-<br>${date.getMonth() > 9 ? date.getMonth() : "0" + date.getMonth()}/${date.getDate()}/${date.getFullYear()}
-<br>${date.getDate()}/${date.getMonth() > 9 ? date.getMonth() : "0" + date.getMonth()}/${date.getFullYear()}`;
-document.getElementById("answer2").innerHTML = date1;
-
-// adapt from https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date
-
-
-/* 3.--------
-*/
-
-
-
-
-//adapt from https://stackoverflow.com/questions/817218/how-to-get-the-entire-document-html-as-a-string#answer-35917295
-
-
-
-/* 4.--------
-*/
-//
-
-
-
-
-
-// adapt from Learning Javascript Ethan Brown Chapter 1
-
-
 // task 1 functions
 function getWeekdayAsString(dayAsInt) {
     switch(dayAsInt) {
@@ -65,7 +27,6 @@ function getWeekdayAsString(dayAsInt) {
 
     }
 }
-
 function getAmPmString(hours) {
     let suffix = "AM";
     if(hours >= 12) {
@@ -77,4 +38,48 @@ function getAmPmString(hours) {
     return `${hours}:${date.getMinutes()}:${date.getSeconds()} ${suffix}`;
 }
 // end task 1 functions
-// task 2 functions
+document.getElementById("answer1").innerHTML = date0;
+//adapt from https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date; https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
+
+
+/* 2.--------
+*/
+
+let date1 = `${date.getMonth() > 9 ? date.getMonth() : "0" + date.getMonth()}-${date.getDate()}-${date.getFullYear()}
+<br>${date.getMonth() > 9 ? date.getMonth() : "0" + date.getMonth()}/${date.getDate()}/${date.getFullYear()}
+<br>${date.getDate()}/${date.getMonth() > 9 ? date.getMonth() : "0" + date.getMonth()}/${date.getFullYear()}`;
+
+document.getElementById("answer2").innerHTML = date1;
+
+// adapt from https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date
+
+
+/* 3.--------
+*/
+
+let ansThree = document.getElementById("answer3");
+const PAGE_HTML = document.documentElement.innerHTML;
+
+
+// task 3 functions
+function myFunction() {
+    ansThree.innerHTML += PAGE_HTML;
+}
+// end task 3 functions
+
+
+//adapt from https://stackoverflow.com/questions/817218/how-to-get-the-entire-document-html-as-a-string#answer-35917295
+
+
+
+/* 4.--------
+*/
+//
+
+
+
+
+
+// adapt from Learning Javascript Ethan Brown Chapter 1
+
+
